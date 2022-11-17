e50a4974e00ef51b500fcdb5ae47fb0bc9304384
Paolo Cisneros <fercis2011@hotmail.com>
2022-11-17 14:26:01 -0500

    fix: files

--- src/main/java/pe/edu/user/userservice/UserServiceApplication.java
+++ src/main/java/pe/edu/user/userservice/UserServiceApplication.java
@@ -2,16 +2,7 @@ package pe.edu.user.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
 @SpringBootApplication
-@EnableFeignClients
-@EnableEurekaClient
-@EnableHystrix
-@EnableHystrixDashboard
 public class UserServiceApplication {
 
     public static void main(String[] args) {
